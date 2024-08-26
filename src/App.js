eb418bf8ea59a1f983a353ee0cdb7dea7da4d15e
david <mangasaryan@gmail.com>
2024-08-26 10:20:07 +0300

    Для описания

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route index element={<Home/>} />
+                <Route index element={<Main/>} />
                 {/*<Route path='/carousel' element = {<Carousel/>}></Route>*/}
                 <Route path="*" element={<NotFound />} />
-                <Route path="/main" element={<Main />} />
+                {/*<Route path="" element={<Main />} />*/}
                 <Route path="/email" element={<Email />} />
             </Routes>
         </BrowserRouter>
